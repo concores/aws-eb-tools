2e7416376507a0858d291e46a9bf647aee8154a5
Yuki TAMURA <yuki.tamura@photoruction.com>
2025-02-28 14:54:00 +0900

    fix

--- login.sh
+++ login.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-EB_ENV=$(./get_eb_envs.sh | fzf)
+EB_ENV=$(./01_get_eb_envs.sh | fzf)
 echo "EB env: ${EB_ENV}"
-EB_INSTANCE=$(./get_instances.sh ${EB_ENV} | fzf)
+EB_INSTANCE=$(./02_get_instances.sh ${EB_ENV} | fzf)
 echo "EB instance: ${EB_INSTANCE}"
-./connect_to_instance.sh ${EB_INSTANCE}
+./03_connect_to_instance.sh ${EB_INSTANCE}
